42230f74a0a0716e6f7dd9e739fc4ae15f9abbf2
Reinier Hernández <sasuke.reinier@gmail.com>
2024-12-09 16:49:28 -0300

    feat(formula): add getmyip formula to get public IP

---
+++ getmyip.rb
@@ -0,0 +1,12 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+class Getmyip < Formula
+  desc "Get my current public IP and copied to the clipboard"
+  homepage ""
+  url "https://github.com/ragnarok22/getmyip/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "96887ca45d431232c7b3562e53dd3a3566da6fd8e87f761f2ce324549195845d"
+  license ""
+
+  test do
+    system "false"
+  end
+end
