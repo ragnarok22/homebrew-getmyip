521a1b60e655ffca9eef1b7aee936f3cc31cc033
Reinier Hernández <sasuke.reinier@gmail.com>
2024-12-09 17:41:01 -0300

    fix(getmyip): update sha256 checksum for version 1.0.0 download URL

--- getmyip.rb
+++ getmyip.rb
@@ -3,7 +3,7 @@ class Getmyip < Formula
   desc "Get my current public IP and copied to the clipboard"
   homepage ""
   url "https://github.com/ragnarok22/getmyip/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "ed3403ccfd3d18c52d6aa1d66809b6d474fe475024f3b7bf40573ad71ec268cf"
+  sha256 "96887ca45d431232c7b3562e53dd3a3566da6fd8e87f761f2ce324549195845d"
   license ""
 
   def install
