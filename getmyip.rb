fefedc1cc0153198b6599b976499ee568c1bd856
Reinier Hernández <sasuke.reinier@gmail.com>
2025-02-02 10:55:35 -0300

    chore(formula): update homepage and license for Getmyip formula

--- getmyip.rb
+++ getmyip.rb
@@ -1,10 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
 class Getmyip < Formula
   desc "Get my current public IP and copied to the clipboard"
-  homepage ""
+  homepage "https://github.com/ragnarok22/getmyip"
   url "https://github.com/ragnarok22/getmyip/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "96887ca45d431232c7b3562e53dd3a3566da6fd8e87f761f2ce324549195845d"
-  license ""
+  license "MIT"
 
   def install
     bin.install "getmyip"
@@ -12,7 +11,7 @@ class Getmyip < Formula
   end
 
   test do
-    output = shell_output("#{bin}/getmyip").strip
+    output = shell_output(bin/"getmyip").strip
     assert_match(/\b\d{1,3}(\.\d{1,3}){3}\b/, output, "Output does not match an IP format")
   end
 end
